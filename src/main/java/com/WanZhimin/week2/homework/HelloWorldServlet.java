2b805ae4494a82e1bf75f20ecd3d9d3815822b72
2019211001001003-WanZhimin <945414215@qq.com>
2021-03-17 13:56:45 +0800

    HW-Week2 Commit

--- src/main/java/com/WanZhimin/week2/homework/HelloWorldServlet.java
+++ src/main/java/com/WanZhimin/week2/homework/HelloWorldServlet.java
@@ -5,13 +5,21 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.Date;
 
 public class HelloWorldServlet extends HttpServlet {
+
+    String name="WanZhimin";
+    String ID="2019211001001003";
+
 
     public void doget (HttpServletRequest request, HttpServletResponse response)
             throws IOException {
         PrintWriter writer = response.getWriter();
-        writer.println("Hello Client !!!");
+        Date date=new Date();
+        writer.println("Name: "+name+"<br/>");
+        writer.println("ID: "+ID+"<br/>");
+        writer.println("Date and Time: "+date.toString()+"<br/>");
     }
 
     public void dopost (HttpServletRequest request, HttpServletResponse response){
